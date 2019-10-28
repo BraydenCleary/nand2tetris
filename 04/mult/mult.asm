ec0b5860dfc536a20977512efecda2fcd21812a5
Brayden Cleary <brayden.cleary@gmail.com>
2019-10-28 13:24:19 -0400

    v1 of mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,51 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@counter
+M=1
+@sum
+M=0
+
+// Return 0 if R1 is 0
+@R1
+D=M
+@SETR2
+D;JEQ
+
+// Return 0 if R0 is 0
+@R0
+D=M
+@SETR2
+D;JEQ
+
+(ADDLOOP)
+  @R1 // this will the the value that gets added to itself R0 times
+  D=M
+  @sum
+  M=D+M
+
+  @R0 // this is the value that determines how many times R1 gets added to itself
+  D=M
+  @counter
+  D=D-M
+  @SETR2
+  D;JEQ
+  @counter
+  M=M+1
+  
+  @ADDLOOP
+  0;JEQ
+
+(SETR2)
+  @sum
+  D=M
+  @R2
+  M=D
+  @END
+  0;JEQ
+
+(END)
+  @END
+  0;JEQ
+  
