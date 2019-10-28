a8a973bf5e6fbfbb99d68e214e52fff338512dcb
Brayden Cleary <brayden.cleary@gmail.com>
2019-10-28 13:31:20 -0400

    mult propgram optimized for loop count

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -16,34 +16,64 @@ M=0
 // Return 0 if R1 is 0
 @R1
 D=M
-@SETR2
+@SETR2ANDEND
 D;JEQ
 
 // Return 0 if R0 is 0
 @R0
 D=M
-@SETR2
+@SETR2ANDEND
 D;JEQ
 
-(ADDLOOP)
-  @R1 // this will the the value that gets added to itself R0 times
+// Determine which value to loop over (want to minimize loop count)
+@R0
+D=M
+@R1
+D=D-M
+// If D is greater than or equal to 0, we want to add R0 to itself R1 times
+@ADDLOOPR1TIMES
+D;JGE
+// If D is less than 0, we want to add R1 to itself R0 times
+@ADDLOOPR0TIMES
+D;JLT
+
+(ADDLOOPR1TIMES)
+  @R0 // This value will get repeatedly added
   D=M
   @sum
   M=D+M
 
-  @R0 // this is the value that determines how many times R1 gets added to itself
+  @R1
   D=M
   @counter
   D=D-M
-  @SETR2
+  @SETR2ANDEND
   D;JEQ
   @counter
   M=M+1
   
-  @ADDLOOP
+  @ADDLOOPR1TIMES
   0;JEQ
 
-(SETR2)
+(ADDLOOPR0TIMES)
+  @R1 // This value will get repeatedly added
+  D=M
+  @sum
+  M=D+M
+
+  @R0
+  D=M
+  @counter
+  D=D-M
+  @SETR2ANDEND
+  D;JEQ
+  @counter
+  M=M+1
+  
+  @ADDLOOPR0TIMES
+  0;JEQ
+
+(SETR2ANDEND)
   @sum
   D=M
   @R2
