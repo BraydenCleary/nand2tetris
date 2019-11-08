7d39632cf390ca084749742f0be726957cf359b2
Brayden Cleary <brayden.cleary@gmail.com>
2019-11-07 19:16:46 -0500

    Writing comments for code generation to help with readability

--- 07/VMTranslator/code_writer.py
+++ 07/VMTranslator/code_writer.py
@@ -13,23 +13,28 @@ class CodeWriter:
       sys_init_bootstrap = ''
 
     general_bootstrap =  (
-        '// Generating bootstrap\n' +
+        '// #Generating bootstrap\n' +
+        '// ~~Setting SP to 256\n' +
         '@256\n' +
         'D=A\n' +
         '@SP\n' +
         'M=D\n' +
+        '// ~~Setting LCL to 5000\n' +
         '@5000\n' +
         'D=A\n' +
         '@LCL\n' +
         'M=D\n' +
+        '// ~~Setting ARG to 6000\n' +
         '@6000\n' +
         'D=A\n' +
         '@ARG\n' +
         'M=D\n' +
+        '// ~~Setting THIS to 6000\n' +
         '@3000\n' +
         'D=A\n' +
         '@THIS\n' +
         'M=D\n' +
+        '// ~~Setting THAT to 6000\n' +
         '@4000\n' +
         'D=A\n' +
         '@THAT\n' +
@@ -71,10 +76,10 @@ class CodeWriter:
 
   def __generate_function(self, function_name, local_variable_count):
     return (
-      '// Generating function\n' +
-      # set label for function
+      '// #Generating function\n' +
+      '// ~~Setting label for function\n' +
       '({})\n'.format(function_name) +
-      # create local variables (implementing a loop here)
+      '// ~~Creating local variables for function (R13 holds number of variables to be created, R14 holds number of variables already created)\n' +
       '@{}\n'.format(local_variable_count) +
       'D=A\n' + 
       '@R13\n' +
@@ -104,7 +109,8 @@ class CodeWriter:
     call_identifier = str(uuid4())
     arg_subtraction_string = 'D=D-1\n' * int(argument_count)
     return (
-      '// Generating call\n' +
+      '// #Generating call\n' +
+      '// ~~Pushing return address onto stack\n' +
       '@RETURN_LABEL_FOR_{}_{}\n'.format(function_name, call_identifier) +
       'D=A\n' +
       '@SP\n' + 
@@ -112,6 +118,7 @@ class CodeWriter:
       'M=D\n' + 
       '@SP\n' + 
       'M=M+1\n' +
+      '// ~~Pushing current LCL address value onto stack\n' +
       '@LCL\n' +
       'D=M\n' +
       '@SP\n' + 
@@ -119,6 +126,7 @@ class CodeWriter:
       'M=D\n' + 
       '@SP\n' + 
       'M=M+1\n' +
+      '// ~~Pushing current ARG address value onto stack\n' +
       '@ARG\n' +
       'D=M\n' +
       '@SP\n' + 
@@ -126,6 +134,7 @@ class CodeWriter:
       'M=D\n' + 
       '@SP\n' + 
       'M=M+1\n' +
+      '// ~~Pushing current THIS address value onto stack\n' +
       '@THIS\n' +
       'D=M\n' +
       '@SP\n' + 
@@ -133,6 +142,7 @@ class CodeWriter:
       'M=D\n' + 
       '@SP\n' + 
       'M=M+1\n' +
+      '// ~~Pushing current THAT address value onto stack\n' +
       '@THAT\n' +
       'D=M\n' +
       '@SP\n' + 
@@ -141,6 +151,7 @@ class CodeWriter:
       '@SP\n' + 
       'M=M+1\n' +
       # ARG = SP - n - 5
+      '// ~~Setting ARG as SP - number of args - 5\n' +
       '@SP\n' +
       'D=M-1\n' +
       'D=D-1\n' +
@@ -150,24 +161,27 @@ class CodeWriter:
       arg_subtraction_string +
       '@ARG\n' +
       'M=D\n' +
+      '// ~~Setting LCL address value as current SP address value\n' +
       '@SP\n' +
-      'D=M\n'+
+      'D=M\n' +
       '@LCL\n' +
-      'M=D\n'+
+      'M=D\n' +
+      '// ~~oto function\n' +
       '@{}\n'.format(function_name) +
       '0;JMP\n' +
+      '// Set a label in the code where execution will resume when function call returns\n' +
       '(RETURN_LABEL_FOR_{}_{})\n'.format(function_name, call_identifier)
     )
 
   def __generate_return(self):
-    # this is so wrong, need to figure out why
     return (
-      '// Generating Frame = LCL\n'
+      '// #Generating return\n' +
+      '// ~~Generating Frame = LCL\n' +
       '@LCL\n' +
       'D=M\n' +
       '@temp_frame\n'+
       'M=D\n' +
-      '// Generating RET = *(FRAME-5)\n'
+      '// ~~Generating RET = *(FRAME-5)\n'
       '@temp_frame\n' +
       'D=M-1\n' +
       'D=D-1\n' +
@@ -178,19 +192,19 @@ class CodeWriter:
       'D=M\n' +
       '@temp_go_to_at_end_of_return\n' +
       'M=D\n'+
-      '// Generating *ARG = pop()\n' +
+      '// ~~Generating *ARG = pop()\n' +
       self.__generate_pop('temp', 0) +
       '@5\n' +
       'D=M\n' +
       '@ARG\n' +
       'A=M\n' +
       'M=D\n' +
-      '// Generating SP = ARG + 1\n' +
+      '// ~~Generating SP = ARG + 1\n' +
       '@ARG\n' +
       'D=M\n' +
       '@SP\n' +
       'M=D+1\n' +
-      '// Generating THAT = *(FRAME-1)\n' +
+      '// ~~Generating THAT = *(FRAME-1)\n' +
       '@temp_frame\n' +
       'D=M\n' +
       'D=D-1\n' +
@@ -198,7 +212,7 @@ class CodeWriter:
       'D=M\n' +
       '@THAT\n' +
       'M=D\n' +
-      '// Generating THIS = *(FRAME-2)\n' +
+      '// ~~Generating THIS = *(FRAME-2)\n' +
       '@temp_frame\n' +
       'D=M\n' +
       'D=D-1\n' +
@@ -207,7 +221,7 @@ class CodeWriter:
       'D=M\n' +
       '@THIS\n' +
       'M=D\n' +
-      '// Generating ARG = *(FRAME-3)\n' +
+      '// ~~Generating ARG = *(FRAME-3)\n' +
       '@temp_frame\n' +
       'D=M\n' +
       'D=D-1\n' +
@@ -217,7 +231,7 @@ class CodeWriter:
       'D=M\n' +
       '@ARG\n' +
       'M=D\n' +
-      '// Generating LCL = *(FRAME-4)\n' +
+      '// ~~Generating LCL = *(FRAME-4)\n' +
       '@temp_frame\n' +
       'D=M\n' +
       'D=D-1\n' +
@@ -235,32 +249,34 @@ class CodeWriter:
 
   def __generate_if(self, label_name):
     return (
-      '// Generating if\n' +
+      '// #Generating if\n' +
       '@SP\n' +
       'A=M-1\n' +
       'D=M\n' +
       '@SP\n' +
       'M=M-1\n' +
+      '// ~~Jump to label if topmost element in stack is not 0 (note: pops topmost element in stack)\n'+
       '@{}\n'.format(label_name) +
       'D;JNE\n'
     )
 
   def __generate_goto(self, label_name):
     return (
-      '// Generating goto\n'
+      '// #Generating goto\n'
       '@{}\n'.format(label_name) +
       '0;JMP\n'
     )
 
   def __generate_label(self, label_name):
     return (
-      '// Generating label\n' +
+      '// #Generating label\n' +
       '({})\n'.format(label_name)
     )
 
   def __generate_add(self):
     return (
-      '// Generating add\n' +
+      '// #Generating add\n' +
+      '// ~~Add two topmost elements in stack and store in SP-2 (decrements SP by 1)\n' +
       '@SP\n' + 
       'A=M-1\n' + 
       'D=M\n' + 
@@ -273,7 +289,8 @@ class CodeWriter:
 
   def __generate_sub(self):
     return (
-      '// Generating sub\n' +
+      '// #Generating sub\n' +
+      '// ~~Subtract topmost element from stack from second to topmost element in stack and store in SP-2 (decrements SP by 1)\n' +
       '@SP\n' + 
       'A=M-1\n' + 
       'D=M\n' + 
@@ -286,7 +303,8 @@ class CodeWriter:
 
   def __generate_neg(self):
     return (
-      '// Generating neg\n' +
+      '// #Generating neg\n' +
+      '// ~~Negates topmost element in stack (SP is unchanged)\n' +
       '@SP\n' + 
       'A=M-1\n' + 
       'D=-M\n' + 
@@ -296,31 +314,37 @@ class CodeWriter:
   def __generate_eq(self):
     eq_identifier = str(uuid4())
     return (
-      '// Generating eq\n' +
+      '// #Generating eq\n' +
+      '// ~~Subtract topmost element from stack from second to topmost element in stack and store in D\n' +
       '@SP\n' + 
       'A=M-1\n' + 
       'D=M\n' + 
       'A=A-1\n' + 
       'D=M-D\n' + 
-      '@IS_EQUAL_{}\n'.format(self.eq_identifier) + 
+      '// ~~If D equals 0, we know the two topmost elements in the stack are equal\n' +
+      '@EQUAL_{}\n'.format(self.eq_identifier) + 
       'D;JEQ\n' + 
-      '@IS_NOT_EQUAL_{}\n'.format(self.eq_identifier) + 
+      '// ~~If D does not equal 0, we know the two topmost elements in the stack are not equal\n' +
+      '@NOT_EQUAL_{}\n'.format(self.eq_identifier) + 
       'D;JNE\n' + 
-      '(IS_EQUAL_{})\n'.format(self.eq_identifier) + 
+      '(EQUAL_{})\n'.format(self.eq_identifier) + 
+      '// ~~For the equal case, set SP-2 to -1 (which is all 1s in binary)\n' +
       '@SP\n' +
       'A=M-1\n' +
       'A=A-1\n' +
       'M=-1\n' +
-      '@EQ_END_{}\n'.format(self.eq_identifier) + 
+      '@EQUAL_END_{}\n'.format(self.eq_identifier) + 
       '0;JMP\n' + 
-      '(IS_NOT_EQUAL_{})\n'.format(self.eq_identifier) + 
+      '(NOT_EQUAL_{})\n'.format(self.eq_identifier) + 
+      '// ~~For the not equal case, set SP-2 to 0\n' +
       '@SP\n' + 
       'A=M-1\n' + 
       'A=A-1\n' +
       'M=0\n' + 
-      '@EQ_END_{}\n'.format(self.eq_identifier) + 
+      '@EQUAL_END_{}\n'.format(self.eq_identifier) + 
       '0;JMP\n' + 
-      '(EQ_END_{})\n'.format(self.eq_identifier) +
+      '(EQUAL_END_{})\n'.format(self.eq_identifier) +
+      '// ~~ecrement SP by 1\n' +
       '@SP\n' +
       'M=M-1\n'
     )
@@ -328,17 +352,21 @@ class CodeWriter:
   def __generate_gt(self):
     gt_identifier = str(uuid4())
     return (
-      '// Generating gt\n' +
+      '// #Generating gt\n' +
+      '// ~~Subtract topmost element from stack from second to topmost element in stack and store in D\n' +
       '@SP\n' +
       'A=M-1\n' +
       'D=M\n' +
       'A=A-1\n' +
       'D=M-D\n' +
+      '// ~~If D is greater than 0, we know that x (second to topmost element in stack) is greater than y (topmost element in stack)\n' +
       '@X_IS_GREATER_{}\n'.format(gt_identifier) +
       'D;JGT\n' +
+      '// ~~If D is less than or equal to 0, we know that x (second to topmost element in stack) is not greater than y (topmost element in stack)\n' +
       '@X_IS_NOT_GREATER_{}\n'.format(gt_identifier) +
       'D;JLE\n' +
       '(X_IS_GREATER_{})\n'.format(gt_identifier) +
+      '// ~~For the greater than case, set SP-2 to -1 (which is all 1s in binary)\n' +
       '@SP\n' +
       'A=M-1\n' +
       'A=A-1\n' +
@@ -346,6 +374,7 @@ class CodeWriter:
       '@GT_END_{}\n'.format(gt_identifier) +
       '0;JMP\n' +
       '(X_IS_NOT_GREATER_{})\n'.format(gt_identifier) +
+      '// ~~For the not greater than case, set SP-2 to 0\n' +
       '@SP\n' +
       'A=M-1\n' +
       'A=A-1\n' +
@@ -353,6 +382,7 @@ class CodeWriter:
       '@GT_END_{}\n'.format(gt_identifier) +
       '0;JMP\n' +
       '(GT_END_{})\n'.format(gt_identifier) +
+      '// ~~Decrement SP by 1\n' +
       '@SP\n' +
       'M=M-1\n'
     )
@@ -360,17 +390,21 @@ class CodeWriter:
   def __generate_lt(self): 
     lt_identifier = str(uuid4())
     return (
-      '// Generating lt\n' +
+      '// #Generating lt\n' +
+      '// ~~Subtract topmost element from stack from second to topmost element in stack and store in D\n' +
       '@SP\n' + 
       'A=M-1\n' + 
       'D=M\n' + 
       'A=A-1\n' + 
       'D=M-D\n' + 
+      '// ~~If D is less than 0, we know that x (second to topmost element in stack) is less than y (topmost element in stack)\n' +
       '@X_IS_LESS_THAN_{}\n'.format(lt_identifier) + 
       'D;JLT\n' + 
+      '// ~~If D is greater than or equal to 0, we know that x (second to topmost element in stack) is not less than y (topmost element in stack)\n' +
       '@X_IS_NOT_LESS_THAN_{}\n'.format(lt_identifier) + 
       'D;JGE\n' + 
       '(X_IS_LESS_THAN_{})\n'.format(lt_identifier) + 
+      '// ~~For the less than case, set SP-2 to -1 (all 1s in binary)\n' +
       '@SP\n' + 
       'A=M-1\n' +
       'A=A-1\n' + 
@@ -378,6 +412,7 @@ class CodeWriter:
       '@LT_END_{}\n'.format(lt_identifier) + 
       '0;JMP\n' + 
       '(X_IS_NOT_LESS_THAN_{})\n'.format(lt_identifier) + 
+      '// ~~For the not less than case, set SP-2 to 0\n' +
       '@SP\n' + 
       'A=M-1\n' + 
       'A=A-1\n' +
@@ -385,25 +420,29 @@ class CodeWriter:
       '@LT_END_{}\n'.format(lt_identifier) + 
       '0;JMP\n' + 
       '(LT_END_{})\n'.format(lt_identifier) +
+      '// ~~Decrement SP by 1\n' +
       '@SP\n' +
       'M=M-1\n'
     )
   
   def __generate_and(self):
     return (
-      '// Generating and\n' +
+      '// #Generating bitwise and\n' +
+      '// ~~Take bitwise and of second to topmost element in stack and topmost element in stack and store in SP-2\n' +
       '@SP\n' +
       'A=M-1\n' +
       'D=M\n' +
       'A=A-1\n' +
       'M=M&D\n' +
+      '// ~~Decrement SP by 1\n' +
       '@SP\n' +
       'M=M-1\n'
     )
   
   def __generate_or(self):
     return (
-      '// Generating or\n' +
+      '// #Generating bitwise or\n' +
+      '// ~~Take bitwise or of second to topmost element in stack and topmost element in stack and store in SP-2\n' +
       '@SP\n' +
       'A=M-1\n' +
       'D=M\n' +
@@ -415,7 +454,8 @@ class CodeWriter:
   
   def __generate_not(self):
     return (
-      '// Generating not\n' +
+      '// #Generating bitwise not\n' +
+      '// ~~Take bitwise not of topmost element in stack\n' +
       '@SP\n' +
       'A=M-1\n' +
       'D=!M\n' +
@@ -466,11 +506,14 @@ class CodeWriter:
       )
 
     return (
-      '// Generating push\n' +
+      '// #Generating push\n' +
+      '// ~~Setting value to be pushed to stack in D\n' +
       value_to_be_pushed + 
+      '// ~~Push value to stack\n' +
       '@SP\n' + 
       'A=M\n' + 
       'M=D\n' + 
+      '// ~~Increment SP by 1\n' +
       '@SP\n' + 
       'M=M+1\n'
     )
@@ -511,20 +554,25 @@ class CodeWriter:
       )
 
     return (
-      '// Generating pop\n' +
+      '// #Generating pop\n' +
+      '// ~~Store topmost element in stack in R13\n' +
       '@SP\n' +
       'A=M-1\n' +
       'D=M\n' +
       '@R13\n' +
       'M=D\n' +
+      '// ~~Setting pop destination in D\n' +
       pop_destination +
+      '// ~~Storing pop destination in R14\n' +
       '@R14\n' +
       'M=D\n' +
       '@R13\n' +
       'D=M\n' +
+      '// ~~Set target A value and move popped item to destination\n' +
       '@R14\n' +
       'A=M\n' +
       'M=D\n' +
+      '// ~~Decrement SP by 1\n' +
       '@SP\n' +
       'M=M-1\n'
     )
